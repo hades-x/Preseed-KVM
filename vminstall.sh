da9308e46a481e5f7d47fb648198f9d8bf52ec39
Hades <81820116+hades-x@users.noreply.github.com>
2024-03-13 13:31:34 +0100

    Update vminstall.sh

--- vminstall.sh
+++ vminstall.sh
@@ -1,11 +1,18 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+# Demande le nom de la VM à l'utilisateur
+echo "Entrez le nom de la VM :"
+read VM_NAME
+
+# Exécute virt-install avec le nom de la VM fourni
 virt-install \
-  --name deb-test \
+  --name "$VM_NAME" \
   --memory 2048 \
   --vcpus 2 \
-  --location /home/hades/Documents/debian-12.iso \
+  --location /home/hades/Documents/debian-12.4.0-amd64-netinst.iso  \
   --os-variant debian10 \
   --initrd-inject=preseed.cfg \
-  --extra-args="ks=file:/preseed.cfg" \
+  --extra-args="auto=true priority=critical preseed/file=/preseed.cfg" \
   --disk size=10,format=qcow2,bus=virtio
+
+echo "Installation de la VM $VM_NAME lancée."
